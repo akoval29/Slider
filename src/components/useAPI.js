b7f6eb1d4da51e0761973bea4fb5589676d84a77
Andriy Koval <akoval29@gmail.com>
2023-04-02 14:26:14 +0300

    ref

--- src/components/useAPI.js
+++ src/components/useAPI.js
@@ -20,7 +20,6 @@ const useAPI = () => {
       "563492ad6f91700001000001ff981c4ab12a4096aa425dfe9d443dd0";
     const API_URL = "https://api.pexels.com/v1/search?query=";
     const url = `${API_URL}${search}&per_page=${perPage}`;
-
     try {
       const { data } = await axios.get(url, {
         headers: { Authorization: ACCESS_TOKEN },
